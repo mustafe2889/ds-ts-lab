e04a3e94693f6c57521f6adb216169b7f5b49096
Mustafe Abdi <20088394@mail.wit.ie>
2024-10-02 01:21:19 +0000

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -14,6 +14,11 @@ interface Colleague{
     
 }
 
+interface ColleagueHistory {
+    current: Colleague[],
+    former: Colleague[]
+  }
+
 const friend1: Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
@@ -56,7 +61,7 @@ const friend1: Friend = {
       extension: 125,
     },
   };
-  const colleagues = {
+  export const colleagues : ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
