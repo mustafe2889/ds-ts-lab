bd91a2fd16fb96167b8c029e398c3224e67fe8cc
Mustafe Abdi <20088394@mail.wit.ie>
2024-10-02 01:20:12 +0000

    Added type-safety to friend AND colleague data.

---
+++ src/01-basics.ts
@@ -0,0 +1,64 @@
+interface Friend {
+    name: string;
+    phone: string;
+    age: number
+}
+interface Contact{
+    email: string;
+    extension: number
+}
+interface Colleague{
+    name: string;
+    department: string;
+    contact: Contact;
+    
+}
+
+const friend1: Friend = {
+    name: "Paul Fleming",
+    phone: "087-12345",
+    age: 25,
+  };
+  
+  const friend2: Friend = {
+    name: "Jane Costello",
+    phone: "086--12345",
+    age: 31,
+  };
+  
+  const friends = [friend1, friend2];
+  console.log(friends[1]);
+  
+  //   -------------------
+  const colleague1: Colleague = {
+    name: "Liam McWatt",
+    department: "Biology",
+    contact: {
+      email: "lmcwatt@company.com",
+      extension: 121,
+    },
+  };
+  
+  const colleague2: Colleague = {
+    name: "Patti Burke",
+    department: "Finance",
+    contact: {
+      email: "pburke@company.com",
+      extension: 132,
+    },
+  };
+  
+  const colleague3: Colleague = {
+    name: "Dean Sullivan",
+    department: "HR",
+    contact: {
+      email: "dos@company.com",
+      extension: 125,
+    },
+  };
+  const colleagues = {
+    current: [colleague1, colleague2, colleague3],
+    former: [],
+  };
+  
+  console.log(colleagues.current[0]);
